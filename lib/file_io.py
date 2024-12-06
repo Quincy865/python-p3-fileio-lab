1d232be05dc42303ea1fbc6af319802dbbaba301
Quincy Michael <michaelkirima865@gmail.com>
2024-12-06 11:55:52 +0300

    Fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    """Writes content to a .txt file, creating it if it doesn't exist or overwriting if it does."""
+    with open(str(file_name) + ".txt", 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Appends content to an existing .txt file."""
+    with open(str(file_name) + ".txt", 'a') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """Reads the content of a .txt file and returns it."""
+    with open(str(file_name) + ".txt", 'r') as file:
+        return file.read()
+
+
